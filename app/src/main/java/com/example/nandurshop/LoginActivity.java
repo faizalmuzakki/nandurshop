dadca753819373764eece85855910797cb5245ba
Faizal Khilmi Muzakki <faizal.muzakki@gmail.com>
2019-05-10 11:17:49 +0700

    form register

--- app/src/main/java/com/example/nandurshop/LoginActivity.java
+++ app/src/main/java/com/example/nandurshop/LoginActivity.java
@@ -1,12 +1,21 @@
 package com.example.nandurshop;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    public void goRegister(View v){
+        Intent regIntent = new Intent(this,RegisterActivity.class);
+        startActivity(regIntent);
     }
 }
