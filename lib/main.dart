f86fee581f2d285375f9030931f1ec69c6685e79
marde12345 <mardefasma123up@gmail.com>
2019-05-16 19:43:57 +0700

    initial flutter

---
+++ lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'pages/home.dart';
+import 'pages/store.dart';
+import 'pages/tanduran.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Nandurshop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _selectedIndex = 0;
+  Home home;
+  Store store;
+  Tanduran tanduran;
+  List<Widget> pages;
+  Widget currentPage;
+
+  @override
+  void initState() {
+    home = Home();
+    store = Store();
+    tanduran = Tanduran();
+    currentPage = home;
+
+    pages = [home,store,tanduran];
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: currentPage,
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (value){
+          setState(() {
+            _selectedIndex = value;
+            currentPage = pages[value];
+          });
+        },
+        items: const<BottomNavigationBarItem>[
+      BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        title: Text('Home'),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.store),
+        title: Text('Toko'),
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.local_florist),
+        title: Text('Tanduran')
+      )
+    ]),
+    );
+  }
+}
---
+++ lib/pages/tanduran.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tanduran extends StatefulWidget {
+  @override
+  _TanduranState createState() => _TanduranState();
+}
+
+class _TanduranState extends State<Tanduran> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+    );
+  }
+}
